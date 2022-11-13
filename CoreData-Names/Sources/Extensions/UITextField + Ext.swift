e583851d568d8622c9cb2f879f6dc81a86e19d66
golikovartyom <golikovartem404@gmail.com>
2022-11-13 15:18:14 +0400

    Build main view

---
+++ CoreData-Names/Sources/Extensions/UITextField + Ext.swift
@@ -0,0 +1,32 @@
+//
+//  UITextField + Ext.swift
+//  CoreData-Names
+//
+//  Created by User on 13.11.2022.
+//
+
+import UIKit
+
+extension UITextField {
+
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+
+    func setLeftIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 10, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 40, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+}
--- CoreData-Names/Sources/Modules/Main/MainViewController.swift
+++ CoreData-Names/Sources/Modules/Main/MainViewController.swift
@@ -10,11 +10,98 @@ import SnapKit
 
 class MainViewController: UIViewController {
 
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.setLeftPaddingPoints(10)
+        textField.placeholder = "Print your name here"
+        textField.backgroundColor = .systemGray5
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        return textField
+    }()
+
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Add name", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        button.clipsToBounds = true
+        button.addTarget(
+            self,
+            action: #selector(addName),
+            for: .touchUpInside
+        )
+        return button
+    }()
+
+    private lazy var table: UITableView = {
+        let table = UITableView(frame: .zero, style: .insetGrouped)
+        table.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: "cell"
+        )
+        table.dataSource = self
+        table.delegate = self
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray6
+        setupNavBar()
+        setupHierarchy()
+        setupLayout()
     }
 
+    private func setupNavBar() {
+        title = "Users"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(textField)
+        view.addSubview(button)
+        view.addSubview(table)
+    }
+
+    private func setupLayout() {
+
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.equalTo(view.snp.leading).offset(20)
+            make.trailing.equalTo(view.snp.trailing).offset(-20)
+            make.height.equalTo(48)
+        }
+
+        button.snp.makeConstraints { make in
+            make.top.equalTo(textField.snp.bottom).offset(20)
+            make.leading.equalTo(view.snp.leading).offset(20)
+            make.trailing.equalTo(view.snp.trailing).offset(-20)
+            make.height.equalTo(48)
+        }
+
+        table.snp.makeConstraints { make in
+            make.top.equalTo(button.snp.bottom).offset(20)
+            make.leading.trailing.bottom.equalTo(view)
+        }
+    }
+
+    @objc private func addName() {
+
+    }
 
 }
 
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
