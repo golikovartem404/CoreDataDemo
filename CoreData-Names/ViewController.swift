af067badcd9716778d847b3206662fe590628456
golikovartyom <golikovartem404@gmail.com>
2022-11-13 15:01:04 +0400

    Initial Commit

---
+++ CoreData-Names/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-Names
+//
+//  Created by User on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
